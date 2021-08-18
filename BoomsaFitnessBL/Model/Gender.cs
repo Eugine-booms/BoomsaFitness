e981e545cebb5fc70e7abcc26d3c4398961a60b5
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-08-18 15:13:46 +0300

    Создана модель пользователя

---
+++ BoomsaFitnessBL/Model/Gender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BoomsaFitnessBL.Model
+{/// <summary>
+/// Пол
+/// </summary>
+    public class Gender
+    {
+        public string Name { get; }
+        /// <summary>
+        /// Создание нового гендера
+        /// </summary>
+        /// <param name="Name">Имя пола</param>
+        public Gender (string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(Gender.Name));
+            }
+            this.Name = name;
+            
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ BoomsaFitnessBL/Model/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BoomsaFitnessBL.Model
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// Дата рождений 
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Вес
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public double Height { get; set; }
+        /// <summary>
+        /// Создание нового пользователы
+        /// </summary>
+        /// <param name="name"> Имя пользователя</param>
+        /// <param name="gender"> Пол </param>
+        /// <param name="birthDate"> Дата рождения</param>
+        /// <param name="weight"> Вес </param>
+        /// <param name="height"> Рост </param>
+        #endregion
+        public User(string name,
+                    Gender gender,
+                    DateTime birthDate,
+                    double weight,
+                    double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Гендер не может быть Null", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate > DateTime.Now)
+            {
+                throw new ArgumentException("Дата рождения не может быть выходить за пределы 1900<дата<now", nameof(birthDate));
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Вес не может быть <=0", nameof(weight));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException("Вес не может быть меньше 0", nameof(height));
+            }
+            #endregion
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name; 
+        }
+    }
+}
